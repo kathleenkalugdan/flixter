7ab028957b2882dc314635ee687ac67668c85f2f
kathleen kalugdan <kay16_alcantara@yahoo.com.ph>
2019-06-16 05:28:40 +0000

    added more static pages and permission to view lesson details

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,13 +1,27 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
   
   def show
   end
 
   private
 
+  def require_authorized_for_current_lesson
+    if !current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You are not currently enrolled on this course'
+    end
+  end
+
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  helper_method :current_course
+  def current_course
+    current_lesson.section.course
+  end
+
 end
